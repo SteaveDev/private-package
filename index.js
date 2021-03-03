34c7e483a771e4e58d1d70daf00e36c618efda95
Steave SUN <steave.sun@edu.univ-paris13.fr>
2021-03-03 14:43:49 +0100

    update

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ function portServer(port, name) {
     console.log(`${name} listening at http://localhost:${port}`)
 }
 
+function usage() {
+    let cpu = osu.cpu
+    let mem = osu.mem
+    cpu.usage()
+        .then(info => {
+            console.log('CPU usage: ' + info + ' %')
+        })
+    mem.used()
+        .then(info => {
+            console.log('RAM usage: ' + (info.usedMemMb * 100 / info.totalMemMb).toFixed(2) + ' %')
+        })
+}
+
 module.exports = {
-	portServer
+	portServer,
+	usage
 }
